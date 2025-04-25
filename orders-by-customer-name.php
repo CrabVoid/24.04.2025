24f060df5ebebb13dcea057e88a12cf8261589fe
Edgars G <Edgars@gmail>
2025-04-25 11:36:08 +0300

    mysql subquery

--- orders-by-customer-name.php
+++ orders-by-customer-name.php
@@ -1,3 +1,32 @@
+<?php
+$dbname = "sql_store";
+$servername = "localhost";
+$username = "RalfsEgle";
+$password = "password";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$name = "";
+
+if (isset($_POST["vards"])) {
+    $name = $_POST["vards"];
+}
+
+$sql = "SELECT * FROM Orders
+WHERE customer_id IN (SELECT customer_id FROM customers where first_name = '$name');";
+$result = $conn->query($sql);
+
+echo "<ul>";
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<li>" . "id: " . $row["customer_id"] . " - order id: " . $row["order_id"];
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,30 +37,9 @@
 </head>
 
 <body>
-
-    <?php
-    $dbname = "sql_store";
-    $servername = "localhost";
-    $username = "RalfsEgle";
-    $password = "password";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT * FROM Orders
-WHERE customer_id IN (SELECT customer_id FROM customers where first_name = 'Elka');";
-    $result = $conn->query($sql);
-
-    echo "<ul>";
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "<li>" . "id: " . $row["customer_id"] . " - order id: " . $row["order_id"];
-        }
-    }
-    ?>
+    <form method="post" action="??">
+        <input type="text" name="vards">
+    </form>
 
 </body>
 
